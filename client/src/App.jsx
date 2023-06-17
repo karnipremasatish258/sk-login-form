c068151d919406ada6ad98c41214ff7e80e1f71a
THAtha@1399 <karnipremasatish258@gmail.com>
2023-06-17 13:28:08 +0530

    animation added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -222,9 +222,7 @@ const App = () => {
             </div>
             <div className="buttons">
               {error && <p>{error}</p>}
-              <Button onLoad={false} type="submit" variant="contained">
-                Sign Up
-              </Button>
+              <Button variant="contained">Sign Up</Button>
               <h1 className="signedup">
                 Already Signed Up? &nbsp;
                 <Link onClick={() => setPage("Sign in")} href="#">
@@ -234,52 +232,50 @@ const App = () => {
             </div>
           </form>
         ) : (
-          <div className="card">
-            <form onSubmit={handleSubmit(onSubmitForm)}>
-              <div className="texts">
-                <TextField
-                  {...register("username", {
-                    required: {
-                      value: true,
-                      message: "username required",
-                    },
-                  })}
-                  className="outlined-basic"
-                  id="username"
-                  label="username"
-                  variant="outlined"
-                />
-                <p>{errors.username?.message}</p>
-              </div>
-              <div className="texts">
-                <TextField
-                  {...register("password", {
-                    required: {
-                      value: true,
-                      message: "password required",
-                    },
-                  })}
-                  className="outlined-basic"
-                  id="password"
-                  label="password"
-                  variant="outlined"
-                  type="password"
-                />
-                <p>{errors.password?.message}</p>
-              </div>
-              <div className="buttons">
-                <Button type="submit" variant="contained">
-                  Sign in
-                </Button>
-                <h1 className="signedup">
-                  New User? &nbsp;
-                  <Link onClick={() => setPage("SignUp")} href="#">
-                    Click here
-                  </Link>
-                </h1>
-              </div>
-            </form>
-          </div>
+          <form onSubmit={handleSubmit(onSubmitForm)}>
+            <div className="texts">
+              <TextField
+                {...register("username", {
+                  required: {
+                    value: true,
+                    message: "username required",
+                  },
+                })}
+                className="outlined-basic"
+                id="username"
+                label="username"
+                variant="outlined"
+              />
+              <p>{errors.username?.message}</p>
+            </div>
+            <div className="texts">
+              <TextField
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "password required",
+                  },
+                })}
+                className="outlined-basic"
+                id="password"
+                label="password"
+                variant="outlined"
+                type="password"
+              />
+              <p>{errors.password?.message}</p>
+            </div>
+            <div className="buttons">
+              <Button type="submit" variant="contained">
+                Sign in
+              </Button>
+              <h1 className="signedup">
+                New User? &nbsp;
+                <Link onClick={() => setPage("SignUp")} href="#">
+                  Click here
+                </Link>
+              </h1>
+            </div>
+          </form>
         )}
       </div>
     </div>
